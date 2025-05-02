5020f49c3b6b128c7a7fc1709e0fc08c2e9eb53d
lucifer <d2f.working@gmail.com>
2025-05-02 21:04:37 +0700

    adding process real time data

--- kafka/entrypoint.sh
+++ kafka/entrypoint.sh
@@ -1,27 +1,18 @@
 #!/bin/bash
 
-# Exit on error
 set -e
 
-
-
-echo "Starting Kafka consumer..."
-
-# Wait for Kafka to be ready
 echo "Waiting for Kafka to be ready..."
 while ! nc -z kafka 29092; do
-  echo "Waiting for Kafka..."
-  sleep 2
+    sleep 1
 done
+echo "Kafka is ready!"
 
-# Wait for Qdrant to be ready
 echo "Waiting for Qdrant to be ready..."
 while ! nc -z qdrant 6333; do
-  echo "Waiting for Qdrant..."
-  sleep 2
+    sleep 1
 done
+echo "Qdrant is ready!"
 
-echo "All services are ready. Starting Kafka consumer..."
-
-# Run the Kafka consumer
-exec python kafka.py
+echo "Starting Kafka consumer..."
+exec python kafka_consumer.py
